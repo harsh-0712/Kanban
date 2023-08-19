37ccfb0e7183a0ff2dc1c0dfd630f9807d766f54
Mahendra Singh <07harsh12solanki@gmail.com>
2023-08-20 04:09:54 +0530

    Introduced React Select

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useRef, useEffect } from 'react'
 import Dropdown from 'react-dropdown';
+import Select from 'react-select'
 import 'react-dropdown/style.css';
 import './topbar.css'
 
@@ -8,12 +9,12 @@ import './topbar.css'
 
 function Topbar({setProperty,setOrder}) {
     const options = [
-        'Status', 'User', 'Priority'
+        {value:'Status',label:'Status'}, {value:'User',label:'User'}, {value:'Priority',label:'Priority'}
     ];
 
     const options2 = [
-        'Priority', 'Name'
-    ]
+        { value: 'Priority', label: 'Priority' }, { value: 'Name', label: 'Name' }
+    ];
     
     const [grp,setGrp]=useState(options[0])
     const [ord,setOrd]=useState(options2[0])
@@ -49,24 +50,74 @@ function Topbar({setProperty,setOrder}) {
                 <div className='ft'>
                     <div><p>Grouping</p></div>
                         <div>
-                          <Dropdown
+                          {/* <Dropdown
                               className='drop'
                               options={options}
                               onChange={(e) => {setProperty(e.value.toLowerCase()); setGrp(e)}}
                               value={grp}
-                              
-                          />
+        
+                          /> */}
+                                  <Select
+                                      value={grp}
+                                      required
+                                      onChange={(e) => { setProperty(e.value.toLowerCase()); setGrp(e) }}
+                                      className='drop'
+                                      styles={{
+                                        //   control: (baseStyles, state) => ({
+                                        //       ...baseStyles,
+                                        //       borderColor: 'black',
+                                        //       backgroundColor: 'rgba(10, 180, 180, 0.2)'
+                                        //   }),
+                                        //   option: (baseStyles, state) => ({
+                                        //       ...baseStyles,
+                                        //       borderColor: state.isFocused ? 'black' : 'red',
+                                        //       backgroundColor: state.isFocused ? 'black' : 'rgba(10, 180, 180, 1)',
+                                        //   }),
+                                        //   singleValue: (baseStyles, state) => ({
+                                        //       ...baseStyles,
+                                        //       backgroundColor: 'rgba(10, 180, 180, 1)'
+                                        //   }),
+                                      }}
+                                      options={options}
+                                    //   className="basic-multi-select inputFields"
+                                      classNamePrefix="select inputFields"
+                                  />
                       </div>
                 </div>
                 <div className='ft'>
                     <div><p>Ordering</p></div>
                         <div>
-                            <Dropdown
+                            {/* <Dropdown
                                 className='drop'
                                 options={options2}
                                 onChange={(e) => {setOrder(e.value.toLowerCase()); setOrd(e) }}
                                 value={ord}
-                            />
+                            /> */}
+                                  <Select
+                                        value={ord}
+                                      required
+                                      onChange={(e) => { setOrder(e.value.toLowerCase()); setOrd(e) }}
+                                      className='drop'
+                                      styles={{
+                                          //   control: (baseStyles, state) => ({
+                                          //       ...baseStyles,
+                                          //       borderColor: 'black',
+                                          //       backgroundColor: 'rgba(10, 180, 180, 0.2)'
+                                          //   }),
+                                          //   option: (baseStyles, state) => ({
+                                          //       ...baseStyles,
+                                          //       borderColor: state.isFocused ? 'black' : 'red',
+                                          //       backgroundColor: state.isFocused ? 'black' : 'rgba(10, 180, 180, 1)',
+                                          //   }),
+                                          //   singleValue: (baseStyles, state) => ({
+                                          //       ...baseStyles,
+                                          //       backgroundColor: 'rgba(10, 180, 180, 1)'
+                                          //   }),
+                                      }}
+                                      options={options2}
+                                    //   className="basic-multi-select inputFields"
+                                      classNamePrefix="select inputFields"
+                                  />
                     </div>
                 </div>
             </div>
